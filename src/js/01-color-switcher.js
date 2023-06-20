4fcd221aaeaef4800c4eec9dab3eddcb8ea25d4c
Iryna220 <ibiriszka951@gmail.com>
2023-06-20 19:40:10 +0200

    update home work 9
    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const btnStart = document.querySelector("[data-start]");
+const btnStop = document.querySelector("[data-stop]"); 
+
+btnStart.addEventListener('click', switchColor);
+btnStop.addEventListener('click', stopSwitchColor);
+
+let timer = null;
+
+function switchColor() {
+    if (timer === 0) {
+        timer = setInterval(() => {
+            document.body.style.background = getRandomHexColor()
+        }, 1000);
+        console.log('good work')
+    }
+}
+function stopSwitchColor() {
+    clearInterval(timer);
+    timer = null;
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
 
