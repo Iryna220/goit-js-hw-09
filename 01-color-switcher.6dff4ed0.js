35ea790e5f2d502d0ea0b2edfa68c9211e0c9f07
Iryna220 <123005698+Iryna220@users.noreply.github.com>
2023-06-20 19:21:52 +0000

    Deploying to gh-pages from @ Iryna220/goit-js-hw-09@df11eaa978f44ca1e34d130fd41203410baa0d2b 🚀

--- 01-color-switcher.6dff4ed0.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");t.addEventListener("click",(function(){0===o&&(o=setInterval((()=>{document.body.style.background=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),console.log("good work"))})),e.addEventListener("click",(function(){clearInterval(o),o=null}));let o=null;
-//# sourceMappingURL=01-color-switcher.6dff4ed0.js.map
---
+++ 01-color-switcher.f09cae96.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");t.addEventListener("click",(function(){o=setInterval((()=>{document.body.style.background=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),console.log("good work")})),e.addEventListener("click",(function(){clearInterval(o)}));let o=null;
+//# sourceMappingURL=01-color-switcher.f09cae96.js.map
